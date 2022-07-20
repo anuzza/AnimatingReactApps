7f0047b28526fdcd8dc2e8257307b73a417658df
anuzza <anuja3580@gmail.com>
2022-07-20 07:02:52 -0500

    using reactTransitionGroup

--- src/App.js
+++ src/App.js
@@ -4,10 +4,11 @@ import "./App.css";
 import Modal from "./components/Modal/Modal";
 import Backdrop from "./components/Backdrop/Backdrop";
 import List from "./components/List/List";
-
+import Transition from "react-transition-group/Transition";
 class App extends Component {
   state = {
     modalisOpen: false,
+    showBlock: false,
   };
 
   showModal = () => {
@@ -22,8 +23,42 @@ class App extends Component {
     return (
       <div className="App">
         <h1>React Animations</h1>
-        <Modal show={this.state.modalisOpen} closed={this.closeModal} />
-        <Backdrop show={this.state.modalisOpen} />
+        <button
+          className="Button"
+          onClick={() =>
+            this.setState((prevState) => ({ showBlock: !prevState.showBlock }))
+          }
+        >
+          Toggle
+        </button>
+        <br />
+        <Transition
+          in={this.state.showBlock}
+          timeout={1000}
+          mountOnEnter
+          unmountOnExit
+        >
+          {(state) => (
+            <div
+              style={{
+                backgroundColor: "red",
+                width: 100,
+                height: 100,
+                margin: "auto",
+                transition: "opacity 1s ease-out",
+                opacity: state === "exiting" ? 0 : 1,
+              }}
+            ></div>
+          )}
+        </Transition>
+
+        {this.state.modalisOpen ? (
+          <Modal show={this.state.modalisOpen} closed={this.closeModal} />
+        ) : null}
+        {this.state.modalisOpen ? (
+          <Backdrop show={this.state.modalisOpen} />
+        ) : null}
+
         <button className="Button" onClick={this.showModal}>
           Open Modal
         </button>
